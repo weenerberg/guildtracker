edee5d73f24c1a9c2acfb27eababa0a129d0dff0
Martin Wennerberg <martin.wennerberg@gmail.com>
2018-09-06 00:43:56 +0200

    Added convert step

--- ocr_reader.py
+++ ocr_reader.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from os import listdir
-from os.path import isfile, isdir, join
+from os import listdir, walk, rename, makedirs
+from os.path import isfile, isdir, join, dirname, basename, splitext, exists, realpath, normpath
+import subprocess
 import logging
 import argparse
 import yaml
@@ -52,7 +53,39 @@ def folders_in(path_to_parent):
 		if isdir(join(path_to_parent,fname)):
 			yield (join(path_to_parent,fname))
 
-forlist(folders_in(SITH_EVENT_PATH)))
+
+for event_type_folder in list(folders_in(BASE_PATH)):
+	event_type = basename(event_type_folder)
+	print("Event type: " + event_type)
+	print("1" + event_type_folder)
+	for event_folder in list(folders_in(event_type_folder)):
+		print("2" + event_folder)
+		event_date = event_folder.split("_")[1]
+		for event_score_type in list(folders_in(event_folder)):
+			print("3" + event_score_type)
+			f = []
+			for (dirpath, dirnames, filenames) in walk(event_score_type):
+				f.extend(join(dirpath, filename) for filename in filenames)
+				break
+			for event_score_file in f:
+				print("4" + event_score_file)
+				
+				filename_w_ext = basename(event_score_file)
+				filename, file_extension = splitext(filename_w_ext)
+
+				input_file = '/'.join(event_score_file.split('\\'))
+				print(input_file)
+				output_file = event_score_type + "/preprocessed/" + filename + "_preprocessed" + file_extension
+				output_file = '/'.join(output_file.split('\\'))
+				print(output_file)
+
+				output_filepath = dirname(realpath(output_file))
+				if not exists(output_filepath):
+					makedirs(output_filepath)
+				cmd = ["C:/Program Files/ImageMagick-7.0.8-Q16/convert", input_file, "-colorspace", "gray", "-negate", "-morphology", "close", "diamond", "-threshold", "55%", output_file]
+				fconvert = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+				stdout, stderr = fconvert.communicate()
+				assert fconvert.returncode == 0, stderr
 # poll dbx folder for batches of screenshots
 
 # for each file in a batch
--- utils.py
+++ utils.py
@@ -16,3 +16,18 @@ def setup_new_datasource_file(headers, filepath, filename):
     newFile = csv.writer(open(filepath + filename, "w"),lineterminator='\n')
     newFile.writerow(headers)
     return newFile
+
+def rename(dir, pattern, titlePattern):
+	for pathAndFilename in glob.iglob(os.path.join(dir, pattern)):
+		title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+		os.rename(pathAndFilename, os.path.join(dir, titlePattern % title + ext))
+
+def replace_substring_in_folder_filenames(folder, replace, replacement):
+	pathiter = (os.path.join(root, filename)
+    for root, _, filenames in os.walk(folder)
+    for filename in filenames)
+
+	for path in pathiter:
+    	newname =  path.replace(replace, replacement)
+    	if newname != path:
+        	os.rename(path, newname)
